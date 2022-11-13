84b352ce92199aa2a7d397d95c00e5a4ede06f47
Sacha YAKOVLEFF <syakovle@e1r11p14.clusters.42paris.fr>
2022-11-13 17:45:03 +0100

    split+strlcat

---
+++ ft_split/ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syakovle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 10:49:57 by syakovle          #+#    #+#             */
+/*   Updated: 2022/11/09 15:56:09 by syakovle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!dst || !src)
+		return (0);
+	while (dst[i] && i < size)
+		i++;
+	j = i;
+	while (i < size - 1 && src[i])
+	{
+		dst[i] = src[i - j];
+		i++;
+	}
+	if (j < size)
+		dst[i] = '\0';
+	return (j + ft_strlen((char *)src));
+}
