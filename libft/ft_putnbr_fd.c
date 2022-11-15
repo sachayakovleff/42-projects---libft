133b13d3ac34e3783ec7e75b0b9bbd0095b94727
Sacha YAKOVLEFF <syakovle@e1r5p15.clusters.42paris.fr>
2022-11-15 17:13:03 +0100

    libft

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syakovle <syakovle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 13:20:42 by syakovle          #+#    #+#             */
+/*   Updated: 2022/11/13 14:42:28 by syakovle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <limits.h>
+#include "libft.h"
+#include <stdio.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		if (n == INT_MIN)
+		{
+			ft_putchar_fd('-', fd);
+			ft_putchar_fd('2', fd);
+			n = 147483648;
+		}
+		else
+		{
+			n = n * -1;
+			ft_putchar_fd('-', fd);
+		}
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
+}
