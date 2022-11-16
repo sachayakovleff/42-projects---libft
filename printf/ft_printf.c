c8cc296722b16b63656c23cb6c0ded4de2c173f1
Sacha YAKOVLEFF <syakovle@e2r7p7.clusters.42paris.fr>
2022-11-16 17:08:21 +0100

    libft fixed + printf

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: syakovle <syakovle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 13:34:21 by syakovle          #+#    #+#             */
-/*   Updated: 2022/11/15 15:15:44 by syakovle         ###   ########.fr       */
+/*   Updated: 2022/11/16 15:07:39 by syakovle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,58 @@
 #include <stdio.h>
 #include "libft.h"
 
+int	ft_getlength(size_t s)
+{
+	int	count;
+
+	count = 0;
+	while (s > 0)
+	{
+		count++;
+		s /= 10;
+	}
+	return (count);
+}
+
+char	*ft_tohexamin(size_t s)
+{
+	char		*str;
+	int			i;
+	const int	length = ft_getlength(s);
+
+	i = 0;
+	str = malloc(sizeof(char) * (length + 1));
+	if (str == NULL)
+		return (NULL);
+	while (i < length)
+	{
+		str[i] = ft_getchar(s, i);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
 void	ft_getstring(char *str, va_list args)
 {
 	if (str[0] == 's')
 		ft_putstr_fd(va_arg(args, char *), 1);
 	if (str[0] == 'c')
 		ft_putchar_fd(va_arg(args, int), 1);
-	// if (str[0] == 'p') // à faire
-		// return (va_arg(args, void *));
+	if (str[0] == 'p')
+		ft_putstr_fd((char *)va_arg(args, void *), 1);
 	if (str[0] == 'd')
 		ft_putnbr_fd(va_arg(args, int), 1);
 	if (str[0] == 'i')
 		ft_putnbr_fd(va_arg(args, int), 1);
 	if (str[0] == 'u')
 		ft_putnbr_fd(va_arg(args, unsigned int), 1);
-	// if (str[0] == 'x')
-	// 	ft_tohexamin(va_arg(args, unsigned int));
-	// if (str[0] == 'X')
-	// 	ft_tohexamaj(va_arg(args, unsigned int));
+	if (str[0] == 'x')
+		ft_putstr_fd(ft_tohexamin(va_arg(args, unsigned int)), 1);
+	if (str[0] == 'X')
+		ft_putstr_fd(ft_tohexamaj(va_arg(args, unsigned int)), 1);
 	if (str[0] == '%')
-		ft_putchar_fd('%', 1); 
+		ft_putchar_fd('%', 1);
 }
 
 int	ft_printf(char *str, ...)
